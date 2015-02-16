5ff819b103df692feb4854b74cc2125c2eec20d1
alex-bezverkhniy <alexander.bezverkhniy@gmail.com>
2015-02-15 22:15:30 -0800

    Added link to manage page, changed directory structure of js

---
+++ src/main/resources/static/js/sample-reports.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // TODO show sample report
+/*
+    var myRactive = new HATEOASRactive({
+        el: '#reports',
+        template: '#reports-template',
+        data: {rows : samplereports, renderMarkdown: function (md) { return converter.makeHtml(md); }}
+    });
+
+    myRactive.restURL = '/rest/reports?page=0&size=10';
+    myRactive.modelListName = 'reports';
+    myRactive.fire( 'init' );
+*/
+});
